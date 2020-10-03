310e4e99dcbdeb989a72598eb0365329fa240322
sudo97 <i.mihnevich@gmail.com>
2020-10-03 19:05:50 +0300

    stupid mistake fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 export default (store) => (mapStateToProps) => (WrappedComponent) => {
   return class HOC extends React.Component {
     componentDidMount() {
